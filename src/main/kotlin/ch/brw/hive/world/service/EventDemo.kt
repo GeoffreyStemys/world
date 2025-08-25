d717b81782f4ddf781deaa73d72ffac72c76ce44
Geoffrey Vincent <geoffrey.vincent@stemys.io>
2025-08-25 14:04:43 +0200

    debug otel

--- src/main/kotlin/ch/brw/hive/world/service/EventDemo.kt
+++ src/main/kotlin/ch/brw/hive/world/service/EventDemo.kt
@@ -1,29 +1,31 @@
-package ch.brw.hive.world.service
-
+//package ch.brw.hive.world.service
+//
 //import org.springframework.stereotype.Component
 //import org.springframework.jms.annotation.JmsListener
-//import mu.KotlinLogging.logger
+//import io.github.oshai.kotlinlogging.KotlinLogging
+//import org.springframework.boot.context.event.ApplicationReadyEvent
+//import org.springframework.context.event.EventListener
 //
 //
 //@Component
 //class EventDemo {
 //
-//    val logger = logger {}
-
-    // https://activemq.apache.org/components/artemis/documentation/latest/perf-tuning.html#tuning-the-vm
-    // https://activemq.apache.org/components/artemis/documentation/latest/paging.html#configuration-2
-
+//  val logger = KotlinLogging.logger {}
+//
+//  // https://activemq.apache.org/components/artemis/documentation/latest/perf-tuning.html#tuning-the-vm
+//  // https://activemq.apache.org/components/artemis/documentation/latest/paging.html#configuration-2
+//
 //  @EventListener(ApplicationReadyEvent::class)
 //  fun setupConfigWhenAppReady() {
 //    Fusesource.fusesource()
 //  }
-
-//    @JmsListener(
-//        destination = "debug",
-//        containerFactory = "containerFactory"
-//    )
-//    fun listener(message: String) {
-//        logger.info("Received message: $message")
-//    }
-
+//
+//  @JmsListener(
+//    destination = "debug",
+//    containerFactory = "containerFactory"
+//  )
+//  fun listener(message: String) {
+//    logger.info{ "Received message: $message" }
+//  }
+//
 //}
--- src/main/kotlin/ch/brw/hive/world/service/WorldController.kt
+++ src/main/kotlin/ch/brw/hive/world/service/WorldController.kt
@@ -1,6 +1,9 @@
 package ch.brw.hive.world.service
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.api.common.Attributes
+import org.springframework.boot.info.BuildProperties
 import org.springframework.core.env.ConfigurableEnvironment
 import org.springframework.http.ResponseEntity
 import org.springframework.scheduling.annotation.Scheduled
@@ -15,47 +18,66 @@ import kotlin.random.Random
 @RestController
 @RequestMapping(value = ["/hello"])
 class WorldController(
-    val environment: ConfigurableEnvironment
+  val environment: ConfigurableEnvironment,
+  val otel: OpenTelemetry,
+  val buildProperties: BuildProperties,
 ) {
 
-    private val logger = KotlinLogging.logger {}
+  val meterName = "debug.otel.metrics.geogeo"
+  val counterName = "custom.counter.geogeo"
 
-    @GetMapping(value = ["/"])
-    fun putConfigUpdate(): ResponseEntity<String> {
-        val message = randomLogs()
-        return ResponseEntity.ok(message + "\n")
+  private val gauge = otel.getMeter(meterName).counterBuilder(counterName)
+    .setDescription("Current debug")
+    .setUnit("items")
+    .build()
+
+  private var attributes: Attributes = Attributes.builder()
+    .put("appName", buildProperties.name)
+    .put("appVersion", buildProperties.version)
+    .build()
+
+  private val logger = KotlinLogging.logger {}
+
+  @GetMapping(value = ["/"])
+  fun get(): ResponseEntity<String> {
+    val message = randomLogs()
+    return ResponseEntity.ok(message + "\n")
+  }
+
+  // @EventListener(ApplicationReadyEvent::class)
+  @Scheduled(initialDelay = 5, fixedDelay = 10, timeUnit = SECONDS)
+  fun doStuffAfterStartup() {
+    // checkEnvironment()
+    while (true) {
+      // randomLogs()
+      logger.info { "add 1 in gauge $meterName $counterName" }
+      gauge.add(1, attributes)
+      sleep(1300)
     }
 
-    // @EventListener(ApplicationReadyEvent::class)
-    @Scheduled(initialDelay = 5, fixedDelay = 10, timeUnit = SECONDS)
-    fun doStuffAfterStartup() {
+  }
 
-        val management = environment
-            .propertySources.map { it.source }
-            .filterIsInstance<Map<String, Any>>()
-            .flatMap { it.entries }
-            .filter { it.key.contains("management") }
+  private fun checkEnvironment() {
+    val management = environment
+      .propertySources.map { it.source }
+      .filterIsInstance<Map<String, Any>>()
+      .flatMap { it.entries }
+      .filter { it.key.contains("management") }
 
-        val otel = environment
-            .propertySources.map { it.source }
-            .filterIsInstance<Map<String, Any>>()
-            .flatMap { it.entries }
-            .filter { it.key.contains("otel") }
+    val otel = environment
+      .propertySources.map { it.source }
+      .filterIsInstance<Map<String, Any>>()
+      .flatMap { it.entries }
+      .filter { it.key.contains("otel") }
 
-        otel.firstOrNull { it.key.contains("logs.end") }?.value
+    otel.firstOrNull { it.key.contains("logs.end") }?.value
+  }
 
-        while (true) {
-            randomLogs()
-            sleep(1500)
-        }
-
-    }
-
-    private fun randomLogs() = when (Random.nextInt(0, 3)) {
-        2 -> "\uD83D\uDEA8 error plaz basic auth".also { logger.error { it } }
-        1 -> "⚠\uFE0F warn plaz basic auth".also { logger.warn { it } }
-        0 -> "\uD83E\uDD73 Congratulation plaz basic auth".also { logger.info { it } }
-        else -> "\uD83E\uDEB2 bug plaz basic auth".also { logger.debug { it } }
-    }
+  private fun randomLogs() = when (Random.nextInt(0, 3)) {
+    2 -> "\uD83D\uDEA8 error plaz basic auth".also { logger.error { it } }
+    1 -> "⚠\uFE0F warn plaz basic auth".also { logger.warn { it } }
+    0 -> "\uD83E\uDD73 Congratulation plaz basic auth".also { logger.info { it } }
+    else -> "\uD83E\uDEB2 bug plaz basic auth".also { logger.debug { it } }
+  }
 
 }
